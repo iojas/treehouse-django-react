6b1f80551fd4b0b23dde7a999488709b5e1067b4
Ojas Kale <ojasok@gmail.com>
2017-09-29 14:32:43 -0500

    User creation drf

--- thoughts/serializers.py
+++ thoughts/serializers.py
@@ -4,9 +4,12 @@ from rest_framework import routers, serializers, viewsets
 from .models import *
 
 class ThoughtsSerializer(serializers.HyperlinkedModelSerializer):
+
+  condition_display = serializers.SerializerMethodField()
+
   class Meta:
     model = Thoughts
-    fields = ('recorded_at','condition','notes')
+    fields = ('recorded_at','condition', 'condition_display', 'notes','user')
     read_only_fields = ('recorded_at',)
 
   def create(self, validated_data):
@@ -14,3 +17,6 @@ class ThoughtsSerializer(serializers.HyperlinkedModelSerializer):
     thought.user = self.context['request'].user
     thought.save()
     return thought
+
+  def get_condition_display(self,obj):
+    return obj.get_condition_display()
